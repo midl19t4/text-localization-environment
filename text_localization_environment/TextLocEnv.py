07f0db7ca48ae649f3199a153efb379f22d8ab4c
MariusDanner <marius@danner.online>
2019-05-15 11:43:13 +0200

    Return Tuple instead of array as state

--- text_localization_environment/TextLocEnv.py
+++ text_localization_environment/TextLocEnv.py
@@ -79,7 +79,7 @@ class TextLocEnv(gym.Env):
 
         info = self.find_positive_actions()
 
-        return self.state, reward, self.done, info
+        return self.state, reward, self.done, {'positive_actions' :info}
 
     def calculate_reward(self, action):
         reward = 0
@@ -324,7 +324,7 @@ class TextLocEnv(gym.Env):
     def compute_state(self):
         penalty = np.float32(self.current_step * self.DURATION_PENALTY)
         warped = self.get_warped_bbox_contents()
-        return np.concatenate((np.array(warped).flatten(), np.array(self.history).flatten(), np.array([penalty])))
+        return (np.array(warped), np.array(self.history), penalty)
 
     def to_one_hot(self, action):
         line = np.zeros(self.action_space.n, np.bool)
