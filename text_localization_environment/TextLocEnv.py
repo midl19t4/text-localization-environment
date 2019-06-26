ac7fd04e144467f9bb107c38336aa1993ad9d204
jonashering <jnshering@gmail.com>
2019-06-26 15:50:50 +0200

    adapt shape of obs

--- text_localization_environment/TextLocEnv.py
+++ text_localization_environment/TextLocEnv.py
@@ -42,7 +42,7 @@ class TextLocEnv(gym.Env):
                            8: self.trigger
                            }
         # 224*224*3 (RGB image) + 9 * 10 (on-hot-enconded history) = 150618
-        self.observation_space = spaces.Tuple([spaces.Box(low=0, high=256, shape=(224,244,3)), spaces.Box(low=0,high=1,shape=(10,10))])
+        self.observation_space = spaces.Tuple([spaces.Box(low=0, high=256, shape=(224,244,3)), spaces.Box(low=0,high=1,shape=(10,9))])
         self.gpu_id = gpu_id
         if type(image_paths) is not list: image_paths = [image_paths]
         self.image_paths = image_paths
