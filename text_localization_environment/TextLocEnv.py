64d4c0ac3127c5f724aaed405de2ad5179f9108a
jonasnoki <jonasnoackgruen@gmail.com>
2020-01-14 15:30:01 +0100

    Multiword localization (#10)
    * prepare reset for multiword localization
    
    * Move ior creation to trigger action
    
    * Calculate iou based on triggered positions
    
    Co-authored-by: Joshua Ziemann <joshua.ziemann@gmail.com>

--- text_localization_environment/TextLocEnv.py
+++ text_localization_environment/TextLocEnv.py
@@ -6,6 +6,7 @@ from PIL import Image, ImageDraw
 from PIL.Image import LANCZOS, MAX_IMAGE_PIXELS
 import numpy as np
 from text_localization_environment.ImageMasker import ImageMasker
+import copy
 
 
 class TextLocEnv(gym.Env):
@@ -27,6 +28,10 @@ class TextLocEnv(gym.Env):
         :type true_bboxes: numpy.ndarray
         :type gpu_id: int
         """
+
+        # initialize self
+        self.done = False
+
         self.action_space = spaces.Discrete(9)
         self.action_set = {0: self.right,
                            1: self.left,
@@ -46,18 +51,15 @@ class TextLocEnv(gym.Env):
 
         self.reward_function = reward_function
 
-        if type(image_paths) is not list: image_paths = [image_paths]
+        if type(image_paths) is not list:
+            image_paths = [image_paths]
         self.image_paths = image_paths
         self.true_bboxes = true_bboxes
 
-        self.seed()
+        self.np_random, _ = seeding.np_random(None)
 
         self.episode_image = Image.new("RGB", (256, 256))
         self.reset()
-
-    def seed(self, seed=None):
-        self.np_random, seed = seeding.np_random(seed)
-        return [seed]
 
     def step(self, action):
         """Execute an action and return
@@ -84,15 +86,15 @@ class TextLocEnv(gym.Env):
         reward = 0
 
         if self.action_set[action] == self.trigger:
-            self.iou = self.compute_best_iou(self.episode_searched_bboxes)
+            self.iou = self.compute_best_iou(self.episode_not_found_bboxes)
 
             if self.reward_function == "single":
-                iou_with_ior = self.compute_best_iou(self.episode_ior_bboxes)
+                iou_with_ior = self.compute_best_iou(self.episode_found_bboxes)
                 reward = self.ETA * (self.iou - (iou_with_ior**2)) - (self.current_step * self.DURATION_PENALTY)
 
             elif self.reward_function == "sum":
                 sum_ior_ious = 0
-                for ior_box in self.episode_ior_bboxes:
+                for ior_box in self.episode_found_bboxes:
                     sum_ior_ious += self.compute_iou(ior_box)
                 reward = self.ETA * (self.iou - (sum_ior_ious**2)) - (self.current_step * self.DURATION_PENALTY)
 
@@ -160,9 +162,19 @@ class TextLocEnv(gym.Env):
         else:
             self.episode_image = Image.fromarray(new_img.astype(np.uint8))
 
+    def current_best_bbox(self, bboxes):
+        best_bbox = None
+        max_iou = 0
+        for box in bboxes:
+            current_iou = self.compute_iou(box)
+            max_iou = max(max_iou, current_iou)
+            if max_iou == current_iou:
+                best_bbox = box
+
+        return best_bbox
+
     def compute_best_iou(self, bboxes):
         max_iou = 0
-
         for box in bboxes:
             max_iou = max(max_iou, self.compute_iou(box))
 
@@ -215,7 +227,6 @@ class TextLocEnv(gym.Env):
 
     def trigger(self):
         self.done = True
-        #self.create_ior_mark()
 
     @staticmethod
     def box_size(box):
@@ -236,45 +247,61 @@ class TextLocEnv(gym.Env):
         if self.box_size(new_box) < MAX_IMAGE_PIXELS:
             self.bbox = new_box
 
-    def reset(self, image_index=None, stay_on_image=False):
+    def reset(self, image_index=None, stay_on_image=False, start_bbox=None, add_random_iors=True):
         """Reset the environment to its initial state (the bounding box covers the entire image"""
+        print("begin reset")
         if not stay_on_image:
             self.history = self.create_empty_history()
             self.episode_image.close()
 
-        if image_index is not None:
-            if not stay_on_image:
+            if image_index is not None:
                 self.episode_image = Image.open(self.image_paths[image_index])
                 self.episode_true_bboxes = self.true_bboxes[image_index]
+            else:
+                random_index = self.np_random.randint(len(self.image_paths))
+                self.episode_image = Image.open(self.image_paths[random_index])
+                self.episode_true_bboxes = self.true_bboxes[random_index]
+
+            self.episode_found_bboxes = []
+            self.episode_not_found_bboxes = copy.copy(self.episode_true_bboxes)
         else:
-            random_index = self.np_random.randint(len(self.image_paths))
-            self.episode_image = Image.open(self.image_paths[random_index])
-            self.episode_true_bboxes = self.true_bboxes[random_index]
+            found_word_bbox = self.current_best_bbox(self.episode_not_found_bboxes)
+            if found_word_bbox:
+                self.episode_found_bboxes.append(found_word_bbox)
+                self.episode_not_found_bboxes.remove(found_word_bbox)
+            if self.done:
+                self.create_ior_mark(((self.bbox[0], self.bbox[1]), (self.bbox[2], self.bbox[3])))
 
         if self.episode_image.mode != 'RGB':
             self.episode_image = self.episode_image.convert('RGB')
 
-        self.bbox = np.array([0, 0, self.episode_image.width, self.episode_image.height])
+        if start_bbox is None:
+            self.bbox = np.array([0, 0, self.episode_image.width, self.episode_image.height])
+        else:
+            self.bbox = start_bbox
+
         self.current_step = 0
         self.state = self.compute_state()
         self.done = False
 
-        self.add_some_iors()
+        if add_random_iors:
+            self.add_random_iors()
 
+        print("finished reset")
         return self.state
 
-    def add_some_iors(self):
+    def add_random_iors(self):
         """Add a random number of IoRs for correctly found bounding boxes"""
-        self.episode_ior_bboxes = []
-        self.episode_searched_bboxes = []
+        self.episode_found_bboxes = []
+        self.episode_not_found_bboxes = []
         number_of_iors = self.np_random.randint(len(self.episode_true_bboxes))
         for bbox_index in range(len(self.episode_true_bboxes)):
             bbox = self.episode_true_bboxes[bbox_index]
             if bbox_index < number_of_iors:
                 self.create_ior_mark(bbox)
-                self.episode_ior_bboxes.append(bbox)
+                self.episode_found_bboxes.append(bbox)
             else:
-                self.episode_searched_bboxes.append(bbox)
+                self.episode_not_found_bboxes.append(bbox)
 
     def render(self, mode='human', return_as_file=False):
         """Render the current state"""
@@ -307,7 +334,7 @@ class TextLocEnv(gym.Env):
 
     def compute_state(self):
         warped = self.get_warped_bbox_contents()
-        return (np.array(warped, dtype=np.float32), np.array(self.history))
+        return np.array(warped, dtype=np.float32), np.array(self.history)
 
     def to_one_hot(self, action):
         line = np.zeros(self.action_space.n, np.bool)
